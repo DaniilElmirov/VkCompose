2316f6fddf675ce8cc3d2c07d99c4387c172ba56
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-02 20:11:20 +0700

    Перенес карточку

--- app/src/main/java/com/elmirov/vkcompose/MainActivity.kt
+++ app/src/main/java/com/elmirov/vkcompose/MainActivity.kt
@@ -3,44 +3,31 @@ package com.elmirov.vkcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.elmirov.vkcompose.ui.theme.PostCard
 import com.elmirov.vkcompose.ui.theme.VkComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            VkComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            VkComposeTheme() {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .padding(8.dp)
+                        .fillMaxSize(),
                 ) {
-                    Greeting("Android")
+                    PostCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VkComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/elmirov/vkcompose/ui/theme/PostCard.kt
@@ -0,0 +1,162 @@
+package com.elmirov.vkcompose.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.elmirov.vkcompose.R
+
+@Composable
+fun PostCard() {
+    Card(
+        modifier = Modifier.verticalScroll(rememberScrollState()),
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.primary),
+    ) {
+        Header()
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            text = stringResource(R.string.template_text),
+        )
+
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.post_content_image),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
+
+        Statistic()
+    }
+}
+
+@Composable
+private fun Header() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+            contentDescription = null,
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column(
+            modifier = Modifier
+                .weight(1f),
+        ) {
+            Text(
+                text = "Название группы",
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Время",
+                color = MaterialTheme.colorScheme.onSecondary,
+            )
+        }
+
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Composable
+private fun Statistic() {
+    Row(
+        modifier = Modifier.padding(8.dp),
+    ) {
+        Row(
+            modifier = Modifier.weight(1f),
+        ) {
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "900")
+        }
+
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "7")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "8")
+            IconWithText(iconResId = R.drawable.ic_like, text = "12")
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary,
+        )
+
+        Spacer(modifier = Modifier.width(4.dp))
+
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DarkPreview() {
+    VkComposeTheme(darkTheme = true) {
+        PostCard()
+    }
+}
+
+@Preview
+@Composable
+private fun LightPreview() {
+    VkComposeTheme(darkTheme = false) {
+        PostCard()
+    }
+}
