ef4f22a923772a65998ccaaefb5fc6dfab0a31e8
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-04 23:39:28 +0700

    Добавил стейты для главного экрана

---
+++ app/src/main/java/com/elmirov/vkcompose/ui/theme/HomeScreenState.kt
@@ -0,0 +1,18 @@
+package com.elmirov.vkcompose.ui.theme
+
+import com.elmirov.vkcompose.domain.Comment
+import com.elmirov.vkcompose.domain.FeedPost
+
+sealed interface HomeScreenState {
+
+    object Initial: HomeScreenState
+
+    data class Posts(
+        val posts: List<FeedPost>,
+    ) : HomeScreenState
+
+    data class Comments(
+        val feedPost: FeedPost,
+        val comments: List<Comment>,
+    ) : HomeScreenState
+}
