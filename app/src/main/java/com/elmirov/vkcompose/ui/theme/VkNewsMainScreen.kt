8ad4a1e86df2002290aad810fc3be48f4270f077
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-04 15:44:40 +0700

    Вынес в отдельную функцию экран "Home"

--- app/src/main/java/com/elmirov/vkcompose/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/elmirov/vkcompose/ui/theme/VkNewsMainScreen.kt
@@ -1,34 +1,21 @@
 package com.elmirov.vkcompose.ui.theme
 
-import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.elmirov.vkcompose.MainViewModel
 import com.elmirov.vkcompose.ui.theme.NavigationItem.Favorite
 import com.elmirov.vkcompose.ui.theme.NavigationItem.Home
 import com.elmirov.vkcompose.ui.theme.NavigationItem.Profile
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(
     viewModel: MainViewModel,
@@ -63,54 +50,12 @@ fun MainScreen(
                 }
             }
         }
-    ) { padding ->
-        Log.d("TAG", "$padding")
+    ) { paddingValues ->
 
-        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
-
-        LazyColumn(
-            contentPadding = PaddingValues(
-                top = 16.dp,
-                start = 8.dp,
-                end = 8.dp,
-                bottom = 88.dp,
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(
-                items = feedPosts.value,
-                key = { it.id }
-            ) { feedPost ->
-
-                val dismissState = rememberDismissState()
-
-                if (dismissState.isDismissed(DismissDirection.EndToStart))
-                    viewModel.delete(feedPost)
-
-                SwipeToDismiss(
-                    modifier = Modifier.animateItemPlacement(),
-                    state = dismissState,
-                    directions = setOf(DismissDirection.EndToStart),
-                    background = {},
-                    dismissContent = {
-                        PostCard(
-                            feedPost = feedPost,
-                            onLikeClickListener = { statisticItem ->
-                                viewModel.updateCount(feedPost = feedPost, item = statisticItem)
-                            },
-                            onShareClickListener = { statisticItem ->
-                                viewModel.updateCount(feedPost = feedPost, item = statisticItem)
-                            },
-                            onViewsClickListener = { statisticItem ->
-                                viewModel.updateCount(feedPost = feedPost, item = statisticItem)
-                            },
-                            onCommentClickListener = { statisticItem ->
-                                viewModel.updateCount(feedPost = feedPost, item = statisticItem)
-                            },
-                        )
-                    },
-                )
-            }
+        when (selectedNavItem) {
+            Home -> HomeScreen(viewModel = viewModel, paddingValues = paddingValues)
+            Favorite -> Text(text = "Favorite")
+            Profile -> Text(text = "Profile")
         }
     }
 }
