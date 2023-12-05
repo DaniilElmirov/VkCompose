8e5733c22b0530296dbb41aabc21ffd2298c1b8e
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-05 21:11:12 +0700

    Указал какие аргументы будут передаваться

--- app/src/main/java/com/elmirov/vkcompose/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/elmirov/vkcompose/navigation/HomeScreenNavGraph.kt
@@ -2,7 +2,9 @@ package com.elmirov.vkcompose.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.elmirov.vkcompose.domain.FeedPost
 
@@ -22,6 +24,11 @@ fun NavGraphBuilder.homeScreenNavGraph(
 
         composable(
             route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(name = Screen.KEY_FEED_POST_ID) {
+                    type = NavType.IntType
+                }
+            ),
         ) { // comments/{feed_post_id}
             val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID) ?: 0
 
