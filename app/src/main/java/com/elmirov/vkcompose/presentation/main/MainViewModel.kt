7d3d091f62cca313730764036a59e40c8bda96e4
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-07 19:03:28 +0700

    Добавил получение токена

--- app/src/main/java/com/elmirov/vkcompose/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/elmirov/vkcompose/presentation/main/MainViewModel.kt
@@ -1,21 +1,27 @@
 package com.elmirov.vkcompose.presentation.main
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.elmirov.vkcompose.presentation.main.AuthState.Authorized
 import com.elmirov.vkcompose.presentation.main.AuthState.Initial
 import com.elmirov.vkcompose.presentation.main.AuthState.NoAuthorized
-import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
+import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthenticationResult
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _authState = MutableLiveData<AuthState>(Initial)
     val authState: LiveData<AuthState> = _authState
 
     init {
-        _authState.value = if (VK.isLoggedIn()) Authorized else NoAuthorized
+        val storage = VKPreferencesKeyValueStorage(application)
+        val token = VKAccessToken.restore(storage)
+
+        val loggedIn = token != null && token.isValid
+        _authState.value = if (loggedIn) Authorized else NoAuthorized
     }
 
     fun performAuthResult(
