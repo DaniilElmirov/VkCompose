de6ca6a27c37d30dd2ea0ea324a01bf8e7ec518f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-11 21:10:12 +0700

    Добавил интерфейс репозитория в domain слой

---
+++ app/src/main/java/com/elmirov/vkcompose/domain/repository/PostRepository.kt
@@ -0,0 +1,24 @@
+package com.elmirov.vkcompose.domain.repository
+
+import com.elmirov.vkcompose.domain.entity.AuthState
+import com.elmirov.vkcompose.domain.entity.Comment
+import com.elmirov.vkcompose.domain.entity.FeedPost
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+interface PostRepository {
+
+    fun getAuthState(): StateFlow<AuthState>
+
+    fun getRecommendations(): StateFlow<List<FeedPost>>
+
+    fun getComments(feedPost: FeedPost): Flow<List<Comment>>
+
+    suspend fun checkAuthState()
+
+    suspend fun loadNextRecommendations()
+
+    suspend fun deletePost(feedPost: FeedPost)
+
+    suspend fun changeLikeStatus(feedPost: FeedPost)
+}
